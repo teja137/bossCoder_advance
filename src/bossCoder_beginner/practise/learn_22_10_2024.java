6c8de4b0db881adb7547f61fc8f459e55dda0d77
teja137 <ajetdream@yahoo.com>
2024-10-22 09:25:25 +0530

    vaid palindrome learning

--- src/bossCoder_beginner/practise/learn_22_10_2024.java
+++
@@ -1,8 +0,0 @@
-package bossCoder_beginner.practise;
-
-public class learn_22_10_2024 {
-    public static void main(String[] args) {
-        System.out.println("learning");
-    }
-    
-}
---
+++ src/bossCoder_beginner/practise/learn_22_10_2024/p125_leetcode_valid_palindrome.java
@@ -0,0 +1,11 @@
+package bossCoder_beginner.practise.learn_22_10_2024;
+
+/**
+ * p125_leetcode_valid_palindrome
+ */
+public class p125_leetcode_valid_palindrome {
+
+    public static void main(String[] args) {
+        System.out.println("universe");
+    }
+}
