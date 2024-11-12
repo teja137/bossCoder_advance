4ba443d2fc12eeb88a0d2bdb43bc164268cabb3c
teja137 <ajetdream@yahoo.com>
2024-11-12 19:06:43 +0530

    learning

--- src/bit_manipulation/Bit_Manipulation.md
+++ src/bit_manipulation/Bit_Manipulation.md
@@ -5,3 +5,7 @@ XOR returns 0 if the two bits are the same (both 0 or both 1). if not 1
 and left shift formal can be like: a+2^b
 
 and right shift formal can be like: a/2^b
+
+a bit is set means 1
+
+a bit is unset means 0
---
+++ src/bit_manipulation/q2.java
@@ -0,0 +1,14 @@
+package bit_manipulation;
+
+public class q2 {
+    public static void main(String[] args) {
+        /*
+         find the first bit from the right least significant bit (LSB) which is set.
+
+         that means if n=18, 10010
+         
+         fund out the least significant bit that is 1
+         */
+    }
+
+}
